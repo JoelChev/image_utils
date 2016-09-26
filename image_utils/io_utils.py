8cb30bcc8ca5d282ccf11170e935e26d884ed683
Joel Cheverie <joelcheverie@hotmail.com>
2016-09-26 14:42:23 -0400

    Made the image cropping more robust for images and gt

--- image_utils/io_utils.py
+++ image_utils/io_utils.py
@@ -3,7 +3,7 @@ import random
 
 class IO_Utils:
 
-    def image_crop(self, im, width, height):
+    def image_crop(self, im, gt, width, height, num_crop):
         """
         :param im: The image (as a numpy array of pixels) to crop.
                     It is assumed to have dimensions:
@@ -11,29 +11,51 @@ class IO_Utils:
                     W = Image width
                     H = Image height
                     D = Image depth (usually 3 for 3 colour channels)
+        :param gt: The ground truth labelling of im (as a numpy array of pixels) to crop.
+                    It is assumed to have dimensions:
+                    (WxHxD) where:
+                    W = Image width
+                    H = Image height
+                    D = Image depth (usually 3 for 3 colour channels)
         :param width: The desired width in the cropped image.
         :param height: The desired height in the cropped image.
-        :return: A randomly sampled cropped portion of the input image.
-                The cropped image will have dimensions:
-                (width x height x D).
+        :return: images: A numpy array of randomly sampled cropped portion of the input image.
+                The cropped images will have dimensions:
+                (width x height x D). There will be num_crop of them. So images has dimension:
+                (width x height x D x num_crop)
+        :return: gt_images: A numpy array of randomly sampled cropped portion of the corresponding ground truth for an input image.
+                The cropped images will have dimensions:
+                (width x height x D). There will be num_crop of them. So gt_images has dimension:
+                (width x height x D x num_crop)
         """
 
-        cropped_Image = None
         x = im.shape[0]
         y = im.shape[1]
-        while (cropped_Image == None):
+        d = im.shape[2]
+        images = np.zeros((width, height, d, num_crop))
+        gt_images = np.zeros_like(images)
+        index_map = np.zeros((x, y))
+        n = 0
+        while n < num_crop:
             i = random.randrange(0, x)
             j = random.randrange(0, y)
+            if index_map[i,j] == 0:
+                index_map[i,j] = 1
+            else:
+                continue
             width_0 = i - (width/2)
             width_1 = i + (width/2)
             height_0 = j - (height/2)
             height_1 = j + (height/2)
             if width_0 > 0 and width_1 < x:
                 if height_0 > 0 and height_1 < y:
-                    cropped_Image = \
+                    images[:,:,:,n]= \
                     im[width_0:width_1, height_0:height_1, :]
+                    gt_images[:,:,:,n] = \
+                    gt[width_0:width_1, height_0:height_1, :]
+                    n += 1
 
-        return cropped_Image
+        return images, gt_images
 
     def image_flip_horizontal(self, im):
         """
@@ -100,20 +122,22 @@ class IO_Utils:
         dimensions = size,size
         return im.resize(dimensions)
 
-# Used for testing:
+#Used for testing:
 
-#     def main(self):
-#         from PIL import Image
-#         im = Image.open('aerial_photo.jpg')
-#         im = self.image_scale(im, 256)
-#         array = np.array(im)
-#         #array = self.image_crop(array, 256, 256)
-#         #array = self.image_rotate(array)
-#         #array = self.image_flip_vertical(im)
-#         print array.shape
-#         im = Image.fromarray(np.uint8(array))
-#         im.show()
-#
-# if __name__ == "__main__":
-#     io_utils = IO_Utils()
-#     io_utils.main()
+    def main(self):
+        from PIL import Image
+        im = Image.open('aerial_photo.jpg')
+        #im = self.image_scale(im, 256)
+        array = np.array(im)
+        n = 6
+        array_of_images,_ = self.image_crop(array, array, 256, 256, n)
+        #array = self.image_rotate(array)
+        #array = self.image_flip_vertical(im)
+        print array_of_images.shape
+        for i in xrange(n):
+            im = Image.fromarray(np.uint8(array_of_images[:,:,:,i]))
+            im.show()
+
+if __name__ == "__main__":
+    io_utils = IO_Utils()
+    io_utils.main()
